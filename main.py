f43340f086168d8ed0c94635989af68e77ed7503
jeenatamang <lamajeena89@gmail.com>
2024-10-20 19:04:59 +0545

    Edited the buttons and colors

--- main.py
+++ main.py
@@ -9,19 +9,35 @@ root.title("Task Manager")
 
 root.config(bg="lightblue")
 root.geometry("500x500")
+placeholder = "Hello! Enter your task here"
 
 
 task_entry = tk.Entry(root, width=40, bg="white", fg="black", font=("comic sans", 12))
+task_entry.insert(0, placeholder)
 task_entry.pack(pady=10)
 
 
+def on_entry_click(event):
+    if task_entry.get() == placeholder:
+        task_entry.delete(0, tk.END) 
+        task_entry.config(fg='black')
+
+
+def on_focusout(event):
+    if task_entry.get() == '':
+        task_entry.insert(0, placeholder)
+        task_entry.config(fg='grey')
+
+task_entry.bind('<FocusIn>', on_entry_click)
+task_entry.bind('<FocusOut>', on_focusout)
+
 priority_var = tk.StringVar(root)
-priority_var.set("Low")  # Default value
+priority_var.set("Low")  
 priority_menu = tk.OptionMenu(root, priority_var, "Low", "Medium", "High")
 priority_menu.pack(pady=5)
 
 
-due_date_entry = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)
+due_date_entry = DateEntry(root, width=12, background='red', foreground='white', borderwidth=2)
 due_date_entry.pack(pady=5)
 
 
@@ -34,14 +50,15 @@ def add_task():
     priority = priority_var.get()
     due_date = due_date_entry.get()
 
-    if task:
+    if task and task != placeholder:
         task_string = f"{task} | Priority: {priority} | Due: {due_date}"
         tasks.insert(tk.END, task_string)
-        task_entry.delete(0, tk.END)  # Clear the task entry
+        task_entry.delete(0, tk.END)
+        on_focusout(None)
     else:
         messagebox.showwarning("Warning", "You must enter a task.")
 
-# Define remove_task function to remove the selected task
+
 def remove_task():
     try:
         task_index = tasks.curselection()[0]
@@ -60,16 +77,16 @@ def mark_task():
         messagebox.showwarning("Warning", "You must select a task to mark as done.")
 
 
-button_style = {"bg": "lightgray", "fg": "black", "font": ("Arial", 10), "borderwidth": 2}
+button_style = {"bg": "lightgray", "fg": "black", "font": ("Comic sans", 10), "borderwidth": 2}
 
 
-add_button = tk.Button(root, text="Add Task", command=add_task, **button_style)
+add_button = tk.Button(root, text="Add a Task", command=add_task, bg="#4CAF50", fg="white", font=("Arial", 10), borderwidth=2) 
 add_button.pack(pady=5)
 
-remove_button = tk.Button(root, text="Remove Task", command=remove_task, **button_style)
+remove_button = tk.Button(root, text="Remove a Task", command=remove_task, bg="red", fg="white", font=("Arial", 10))
 remove_button.pack(pady=5)
 
-mark_button = tk.Button(root, text="Mark Task as Done", command=mark_task, **button_style)
+mark_button = tk.Button(root, text="Mark Task as Done", command=mark_task, bg="white", fg="black", font=("Arial", 10), borderwidth=2)
 mark_button.pack(pady=5)
 
 
